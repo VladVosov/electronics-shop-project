ec1a02bde42ab700ed3da2d973f99f46473a653b
vlad_vosov <vlad.shvetesov@gmail.com>
2023-06-07 00:21:56 +1000

    homework 1

--- tests/test_item.py
+++ tests/test_item.py
@@ -1,1 +1,13 @@
-"""Здесь надо написать тесты с использованием pytest для модуля item."""
+from src import item
+
+test = item.Item('nokia', 150, 3)
+def test_item():
+    assert test.name == 'nokia'
+    assert test.price == 150
+    assert test.quantity == 3
+
+def test_calculate_total_price():
+    assert item.Item.calculate_total_price(test) == 450
+
+def test_apply_discount():
+    assert item.Item.apply_discount(test) == 150

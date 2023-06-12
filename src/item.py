faab875004f88cea6ea2e7a9470e709e2ad801fb
vlad_vosov <vlad.shvetesov@gmail.com>
2023-06-12 20:16:40 +1000

    HomeWork#2

--- src/item.py
+++ src/item.py
@@ -1,3 +1,6 @@
+import csv
+
+
 class Item:
     """
     Класс для представления товара в магазине.
@@ -9,10 +12,27 @@ class Item:
         """
         Создание экземпляра класса item.
         """
-        self.name = name
+        self.__name = name
         self.price = price
         self.quantity = quantity
         Item.all.append(self)
+
+    @property
+    def name(self):
+        return self.__name
+
+    @name.setter
+    def name(self, name):
+        if len(name) <= 10:
+            self.__name = name
+            return self.__name
+        else:
+            return 'Product name more than 10 characters'
+
+    @staticmethod
+    def string_to_number(number):
+        number = int(number[0])
+        return number
 
     def calculate_total_price(self) -> float:
         """
@@ -21,10 +41,21 @@ class Item:
         all_price = self.price * self.quantity
         return all_price
 
-
-    def apply_discount(self) -> None:
+    def apply_discount(self) -> float:
         """
         Применяет установленную скидку для конкретного товара.
         """
         sale_price = self.pay_rate * self.price
         return sale_price
+
+    @classmethod
+    def instantiate_from_csv(cls) -> None:
+        """Инициализирует экземпляры класса Item данными из файла src/items.csv"""
+        cls.all = []
+        with open('../src/items.csv', 'r', encoding="utf-8") as file:
+            csv_data = csv.DictReader(file)
+            for data in csv_data:
+                name = data['name']
+                price = float(data['price'])
+                quantity = int(data['quantity'])
+                cls(name, price, quantity)
